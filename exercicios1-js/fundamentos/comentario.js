26921c3a7d7fdd63108d6cea08db7c70e02e6ab5
Vinícius Ingracio <viniciusingracio@gmail.com>
2018-08-24 01:01:01 -0300

    Upando arquivos do curso

---
+++ exercicios1-js/fundamentos/comentario.js
@@ -0,0 +1,16 @@
+//Comentário de uma linha
+console.log('linha 1')
+
+/*
+Comentário de múltiplas
+linhas
+*/
+console.log('Linha 2')
+
+/*
+*Comentário de
+*múltiplas linhas
+*
+*
+*/
+console.log('linha 3')
